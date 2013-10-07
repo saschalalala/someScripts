1876b8c06259aa537184cff725b329fca3ea01bb
saschalalala <sascha.zantis@gmail.com>
2013-10-07 17:48:26 +0200

    symlinks in git are kind of dumb

--- comuniocalc.sh
+++ comuniocalc.sh
@@ -1,1 +1,18 @@
-/home/sash/bin/comuniocalc.sh
+#!/bin/zsh
+
+# You have to copy the content of your comunio news page manually (No idea how to crawl it automatically), and copy it to a file
+# This is the german version ("von" and "zu" in $FROMPLAYER and $TOPLAYER have to be updated.
+# I think, this is zsh-only. I will write a python3 version soon.
+
+PLAYER="username"
+FROMPLAYER="von $PLAYER"
+TOPLAYER="zu $PLAYER"
+FILE="/tmp/comunio"
+POINTS=35
+STARTBUDGET=20000000
+
+FROM=$(sed "s:\.::g" $FILE | grep $FROMPLAYER | grep -o "[0-9]*" | awk '{ SUM += $1} END { print SUM }')
+TO=$(sed "s:\.::g" $FILE | grep $TOPLAYER | grep -o "[0-9]*" | awk '{ SUM += $1} END { print SUM }') 
+
+(( ERG=$STARTBUDGET+$FROM-$TO+$POINTS*10000 ))
+echo $ERG
--- mobile.sh
+++ mobile.sh
@@ -1,1 +1,22 @@
-/home/sash/bin/mobile.sh
+#!/bin/bash
+
+# Just checks, if dropbox or owncloud are running.
+# I created that script because I was online a lot via smartphone AP and wanted to minimize background traffic
+
+if [ -z "$(pgrep dropbox)" ]; then
+	echo "No running dropbox instance found"
+else
+	echo "Dropbox is running"
+	killall dropbox
+	echo "Dropbox killed"
+fi
+
+if [ -z "$(pgrep owncloud)" ]; then
+	echo "No running owncloud instance found"
+else
+	echo "Owncloud is running"
+	killall owncloud
+	echo "Owncloud killed"
+fi
+
+
